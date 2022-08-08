0dbfc3a816e7ae19a9d6b15f0caff290af4c0fc1
LoisChen68 <a8283626@gmail.com>
2022-08-08 22:13:27 +0800

    feat: add restaurant seeder

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Restaurant = require('../restaurant')
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+
+  for (let i = 0; i < 10; i++) {
+    Restaurant.create({ name: `name-${i}` })
+  }
+
+  console.log('done.')
+})
