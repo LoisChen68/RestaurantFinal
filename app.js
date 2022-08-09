4dbd4ad8fb55cac2eafff17ef3d5115faa2ccaff
LoisChen68 <a8283626@gmail.com>
2022-08-09 23:05:23 +0800

    refactor: search routes

--- app.js
+++ app.js
@@ -35,15 +35,6 @@ app.use(routes)
 app.use(express.static('public'))
 
 
-//設定搜尋功能
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword.trim().toLowerCase()
-  const filterRestaurant = restaurantData.filter(data => {
-    return data.name.trim().toLowerCase().includes(keyword) ||
-      data.category.includes(keyword)
-  })
-  res.render('index', { restaurantData: filterRestaurant, keyword })
-})
 
 //設定監聽
 app.listen(port, () => {
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -12,4 +12,20 @@ router.get('/', (req, res) => {
     .catch(error => console.error(error))
 })
 
+
+//設定搜尋功能
+router.get('/search', (req, res) => {
+  const keyword = req.query.keyword.trim().toLowerCase()
+  Restaurant.find()
+    .lean()
+    .then(restaurantData => {
+      const filterRestaurant = restaurantData.filter(data => {
+        return data.name.trim().toLowerCase().includes(keyword) ||
+          data.category.includes(keyword)
+      })
+      res.render('index', { restaurants: filterRestaurant, keyword })
+    })
+    .catch(error => console.log(error))
+})
+
 module.exports = router
