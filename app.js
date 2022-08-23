9e874ab1d43c98b46d4a153434f278a0b0666b7b
LoisChen68 <a8283626@gmail.com>
2022-08-23 22:11:34 +0800

    feat: 新增登入、註冊頁

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 //載入handlebars
-const exphbs = require('express-handlebars')
+const hbs = require('express-handlebars')
 
 //引用路由器
 const routes = require('./routes')
@@ -20,8 +20,8 @@ const port = 3000
 // const restaurantData = require('./restaurant.json').results
 
 //設定模板引擎
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+app.engine('hbs', hbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 //使用 bodyParser，解析資料類型
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -38,5 +38,5 @@ app.use(express.static('public'))
 
 //設定監聽
 app.listen(port, () => {
-  console.log(`Express is listening on localhost:${port}`)
+  console.log(`Express is listening on http://localhost:${port}`)
 })
